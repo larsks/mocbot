dbeb001c4fec635898f96bee3a32f1318c81bf3d
Lars Kellogg-Stedman <lars@oddbit.com>
2020-10-29 19:26:41 -0400

    allow setting gunicorn log level from environ

--- start-webhook-receiver.sh
+++ start-webhook-receiver.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 : ${GH_HOOK_PORT:=8080}
+: ${GH_LOG_LEVEL:=info}
 
-exec gunicorn -b 0.0.0.0:${GH_HOOK_PORT} github_hook_receiver:app
+exec gunicorn \
+	-b 0.0.0.0:${GH_HOOK_PORT} \
+	--log-level ${GH_LOG_LEVEL} \
+	--access-logfile /dev/stderr \
+	github_hook_receiver:app
